e606ea084dbf0d1f527a77156c2e81f21ecbbfe9
Rajnish kumar <98949936+rajnishpark03@users.noreply.github.com>
2024-10-09 13:39:19 +0530

    Image search app

---
+++ script.js
@@ -0,0 +1,31 @@
+const accesKey = "W1qwWo4IACpJbzZwHt5jqh9Rv0UpcQ5JAnweev7bFoc";
+const apiUrl = "https://api.unsplash.com/photos/random";
+const imageContainer = document.getElementById("imageContainer");
+
+document.getElementById("searchImages").addEventListener("click"&&"press", () => {
+    const searchQuery = document.getElementById("imageNameInput").value.trim();
+
+    if (searchQuery === "") {
+        alert('Please enter image name here');
+        return;
+    }
+
+    fetch(`${apiUrl}?query=${searchQuery}&count=20&client_id=${accesKey}`)
+
+    .then(response => response.json())
+    .then(data => {
+        imageContainer.innerHTML = "";
+
+        data.forEach(photoData => {
+            const photoUrl = photoData.urls.regular;
+
+            const imageElement = document.createElement('img');
+            imageElement.src = photoUrl;
+            imageElement.className = "img-thumbnail";
+            imageElement.style.height = "200px";
+            imageElement.style.width = "200px";
+
+            imageContainer.appendChild(imageElement);
+        });
+    })
+})
